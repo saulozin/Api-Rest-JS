6a9ad09274e223d559026898877c45cffbf3e163
Saulo Maia <saulomaia2@gmail.com>
2025-04-23 01:51:44 -0400

    Adicionado CORS

--- src/app.js
+++ src/app.js
@@ -3,12 +3,30 @@ import {resolve} from 'path';
 dotenv.config();
 
 import './database';
+
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
+
+const whiteList = [
+  'http://localhost:3001'
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 class App {
   constructor() {
@@ -18,6 +36,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({extend: true}));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, '..', 'uploads')));
